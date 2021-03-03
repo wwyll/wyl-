467fc7fed2cd5e1d63b8fdefc529b1a7498a3c6d
wwyll <79839151+wwyll@users.noreply.github.com>
2021-03-03 20:54:33 +0800

    Add files via upload

---
+++ myMath/myArithmetic.py
@@ -0,0 +1,14 @@
+def myAdd(x, y):
+    return x + y
+
+def mySub(x, y):
+    return x -y
+
+def myMultiply(x, y):
+    return x*y
+
+def myDiv(x, y):
+    if y != 0:
+        return x/y
+    else:
+        return False
---
+++ myMath/myCalcArea.py
@@ -0,0 +1,9 @@
+PI = 3.1415
+def RectArea( w, l):
+  return w * l
+def TriArea( s, h):
+  return s * h / 2
+def SquareArea( s):
+  return s * s;
+def CircleArea( r ):
+  return PI * r * r
